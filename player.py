a9f60e7f9722417db2b652217c10f75070881551
Denis Paterson <denisp@synopsys.com>
2021-11-08 09:51:51 +0000

    Test checking with print statements

--- player.py
+++ player.py
@@ -3,6 +3,8 @@ class Player:
     VERSION = "v0.1"
 
     def betRequest(self, game_state):
+        #print("round = ", game_state.round)
+        #print("bet_index = ", game_state.bet_index)
         return 0
 
     def showdown(self, game_state):
