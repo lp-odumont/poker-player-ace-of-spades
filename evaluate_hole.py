54812e55335ce025041eef320f45d079fb9e5dd3
Andreas Wieferink <andreasw@synopsys.com>
2021-11-08 13:18:45 +0100

    update eval_hole

--- evaluate_hole.py
+++ evaluate_hole.py
@@ -1,3 +1,5 @@
+from card_utils import convert_card_to_int
+
 def get_hand_strength(game_state):
     print("Caclulate")
     id = game_state["in_action"]
@@ -7,10 +9,25 @@ def get_hand_strength(game_state):
     hole_0 = hole[0]
     hole_1 = hole[1]
     is_same = hole_0["suit"] == hole_1["suit"]
-    h_0 = hole_0["rank"]
-    h_1 = hole_0["rank"]
+    h_0 = convert_card_to_int(hole_0["rank"])
+    h_1 = convert_card_to_int(hole_1["rank"])
+    
+    score = 0
+    if not is_same:
+        score += 10
+    if h_0 < h_1:
+        temp = h_1
+        h_1 = h_0
+        h_0 = temp
 
-    print(" %s"%hole_0)
-    print(hole_1)
+    if h_0 != h_1:
+        score +=10
+    
+    score += 15-h_0
+    
+
+    print("hole 0 %s"%hole_0)
+    print("hole 1 %s"%hole_1)
+    print("score %d"%score)
 
     return 18
